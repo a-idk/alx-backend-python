8310dd585c66711fa31115c37bef89b33e4015c2
@a_idk <idoko_research@outlook.com>
2024-10-11 02:55:03 +0100

    Variable annotations updated

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -6,6 +6,6 @@ Author: @a_Idk
 from typing import List, Union
 
 
-def sum_list(mxd_lst: List[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ Fxn takes a list of int & floats & returns their sum """
     return float(sum(mxd_lst))
