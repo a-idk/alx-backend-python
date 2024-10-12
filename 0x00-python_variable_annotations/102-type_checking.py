4202923deb930ef7ba713daea962f393a8001195
@a_idk <idoko_research@outlook.com>
2024-10-12 11:53:49 +0100

    mypy update

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,20 +3,18 @@
 Title: Duck typing - first element of a sequence
 Author: @a_Idk
 """
-from typing import List, Tuple, Any
+from typing import List, Tuple
 
 
-def zoom_array(lst: List[Any], factor: int = 2) -> List[Any]:
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
     """ fxn that zooms array """
-    zoomed_in: List[Any] = [
+    zoomed_in: List[int] = [
             item for item in lst
             for i in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
-
+array = (12, 72, 91)
 zoom_2x = zoom_array(array)
-
 zoom_3x = zoom_array(array, 3)
